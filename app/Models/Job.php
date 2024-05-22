d3d888a021f49ff21ba32d56cf3d9c1c6837083c
MarcOZuPe <138830718+MarcOZuPe@users.noreply.github.com>
2024-05-22 14:38:41 -0500

    clase 10

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,10 +1,14 @@
 <?php
  namespace App\Models;
- use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory as FactoriesHasFactory;
+use Illuminate\Database\Eloquent\Model;
  use Illuminate\Support\Arr ;
+ use Iluminate\Database\Eloquent\Factories\HasFactory;
 
  class Job extends Model{
     
+    use  FactoriesHasFactory   ;
     protected $table = 'job_listings';
 
     protected $fillable = [
