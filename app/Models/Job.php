9a54cb9cf1285c6fcf5c3eda4a356ef7e29ba56d
MarcOZuPe <138830718+MarcOZuPe@users.noreply.github.com>
2024-05-28 22:19:11 -0500

    clase 11

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,19 +1,19 @@
 <?php
- namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory as FactoriesHasFactory;
+namespace App\Models;
+
 use Illuminate\Database\Eloquent\Model;
- use Illuminate\Support\Arr ;
- use Iluminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
- class Job extends Model{
-    
-    use  FactoriesHasFactory   ;
+class Job extends Model {
+    use HasFactory;
+
     protected $table = 'job_listings';
 
-    protected $fillable = [
-        'title',
+    protected $fillable = ['title', 'salary'];
 
-        'salary'
-    ];
-    }   
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
+}
